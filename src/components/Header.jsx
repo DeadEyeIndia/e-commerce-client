3f902d8b3e7ad57fbc779a45475722c8ae755c19
DeadEyeIndia <rahulbattin24@gmail.com>
2022-10-28 20:18:58 +0530

    Header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "../styles/headers.scss";
+import Logo from "../assets/Logo.svg";
+import Profile from "../assets/Profile.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="app__header f-sb-c">
+      <Link to="/" className="app__header-logo">
+        <img src={Logo} className="app__logo" />
+      </Link>
+      <div className="app__header-authenticate">
+        <span>Login</span>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
