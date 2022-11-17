dc62b19e88159368fa9ef52968734b8df3b09810
DeadEyeIndia <rahulbattin24@gmail.com>
2022-11-17 21:48:49 +0530

    header and user authentication

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
+import { signOut } from "../features/auth/authAction";
 import "../styles/headers.scss";
 import Logo from "../assets/Logo.svg";
-import Profile from "../assets/Profile.svg";
+import Profile from "../assets/ProfileLOGO.svg";
+import HandburgerMenu from "../assets/Hand.svg";
+import Search from "../assets/search.svg";
+import CartIcon from "../assets/carticon.png";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+
+  const userFirstName = user?.user?.name.split(" ");
+
+  const handleSignOut = () => {
+    dispatch(signOut());
+  };
+
   return (
     <div className="app__header f-sb-c">
-      <Link to="/" className="app__header-logo">
-        <img src={Logo} className="app__logo" />
-      </Link>
-      <div className="app__header-authenticate">
-        <span>Login</span>
+      <div className="app__header-top">
+        <Link to="/" className="app__header-logo">
+          <img src={Logo} className="app__logo" />
+        </Link>
+
+        {user?.user ? (
+          <div className="app__header-authenticated" onClick={handleSignOut}>
+            <span>{userFirstName[0]}</span>
+            <div className="app__header-user">
+              <img src={Profile} alt="" />
+            </div>
+          </div>
+        ) : (
+          <Link to="/signin" className="app__header-authenticate">
+            <span className="app__signin">Login</span>
+          </Link>
+        )}
+      </div>
+      <div className="app__header-bottom">
+        <img src={HandburgerMenu} alt="handburgermenu" />
+        <div className="app__header-searchbox">
+          <div className="searchIcon f-c-c">
+            <img src={Search} alt="search icon" />
+          </div>
+          <form className="app__header-searchboxInner">
+            <input type="text" />
+            <input type="submit" hidden />
+          </form>
+        </div>
+        <div className="app__cartIcon">
+          <img src={CartIcon} alt="cart icon" />
+        </div>
       </div>
     </div>
   );
---
+++ src/pages/SigninPage.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { signIn } from "../features/auth/authAction";
+import "../styles/signin.scss";
+import Logo from "../assets/Logo.svg";
+
+const SigninPage = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSignInSubmit = (e) => {
+    e.preventDefault();
+
+    if (user.email && user.password) {
+      dispatch(signIn(user.email, user.password));
+    }
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/");
+    }
+  }, [isAuthenticated, navigate]);
+
+  return (
+    <div className="app__signin f-c-c">
+      <Link to="/" className="app__logo-image">
+        <img src={Logo} />
+      </Link>
+      <div className="app__signin-container f-c-c">
+        <form className="app__signin-form f-c-c" onSubmit={handleSignInSubmit}>
+          <h1>LOGIN</h1>
+          <div className="app__signin-innerform">
+            <div className="app__sigin-input_container">
+              <label>Email</label>
+              <input
+                type="email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+              />
+            </div>
+            <div className="app__sigin-input_container">
+              <label>Password</label>
+              <input
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+              />
+            </div>
+            <div className="app__sigin-input_container">
+              <input type="submit" value="Login" />
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="app__signin-forgot_signup">
+        <span>Forgot password?</span>
+        <Link to="/signup">New user? Register here!</Link>
+      </div>
+    </div>
+  );
+};
+
+export default SigninPage;
